c165cc08b6f9246275d5f8cc303cdb961e0f9cfe
Mark Butler <mark@tussockinnovation.co.nz>
2017-10-03 15:57:43 +1300

    more revert

--- es/sagas.js
+++ es/sagas.js
@@ -27,7 +27,7 @@ var _actionTypes = require('./actionTypes');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var _marked = /*#__PURE__*/_regenerator2.default.mark(garbageCollector);
+var _marked = [garbageCollector].map(_regenerator2.default.mark);
 /* global Generator */
 
 // Generator type parameters are: Generator<+Yield,+Return,-Next>
@@ -65,174 +65,166 @@ function garbageCollector() {
           return _context.stop();
       }
     }
-  }, _marked, this);
+  }, _marked[0], this);
 }
 
 var apiGeneric = exports.apiGeneric = function apiGeneric(apiClient) {
-  return (/*#__PURE__*/_regenerator2.default.mark(function _apiGeneric(action) {
-      var _action$payload, method, path, params, data, fetchConfig, _action$meta, success, failure, meta, response;
+  return _regenerator2.default.mark(function _apiGeneric(action) {
+    var _action$payload, method, path, params, data, fetchConfig, _action$meta, success, failure, meta, response;
 
-      return _regenerator2.default.wrap(function _apiGeneric$(_context2) {
-        while (1) {
-          switch (_context2.prev = _context2.next) {
-            case 0:
-              _action$payload = action.payload, method = _action$payload.method, path = _action$payload.path, params = _action$payload.params, data = _action$payload.data, fetchConfig = _action$payload.fetchConfig;
-              _action$meta = action.meta, success = _action$meta.success, failure = _action$meta.failure;
-              meta = (0, _extends3.default)({}, action.meta, {
-                fetchTime: Date.now()
-              });
-              _context2.prev = 3;
-              _context2.next = 6;
-              return (0, _effects.call)(apiClient[method], path, { params: params, data: data, fetchConfig: fetchConfig });
+    return _regenerator2.default.wrap(function _apiGeneric$(_context2) {
+      while (1) {
+        switch (_context2.prev = _context2.next) {
+          case 0:
+            _action$payload = action.payload, method = _action$payload.method, path = _action$payload.path, params = _action$payload.params, data = _action$payload.data, fetchConfig = _action$payload.fetchConfig;
+            _action$meta = action.meta, success = _action$meta.success, failure = _action$meta.failure;
+            meta = (0, _extends3.default)({}, action.meta, {
+              fetchTime: Date.now()
+            });
+            _context2.prev = 3;
+            _context2.next = 6;
+            return (0, _effects.call)(apiClient[method], path, { params: params, data: data, fetchConfig: fetchConfig });
 
-            case 6:
-              response = _context2.sent;
-              _context2.next = 9;
-              return (0, _effects.put)({ meta: meta, type: success, payload: response });
+          case 6:
+            response = _context2.sent;
+            _context2.next = 9;
+            return (0, _effects.put)({ meta: meta, type: success, payload: response });
 
-            case 9:
-              _context2.next = 15;
-              break;
+          case 9:
+            _context2.next = 15;
+            break;
 
-            case 11:
-              _context2.prev = 11;
-              _context2.t0 = _context2['catch'](3);
-              _context2.next = 15;
-              return (0, _effects.put)({ meta: meta, type: failure, payload: _context2.t0, error: true });
+          case 11:
+            _context2.prev = 11;
+            _context2.t0 = _context2['catch'](3);
+            _context2.next = 15;
+            return (0, _effects.put)({ meta: meta, type: failure, payload: _context2.t0, error: true });
 
-            case 15:
-            case 'end':
-              return _context2.stop();
-          }
+          case 15:
+          case 'end':
+            return _context2.stop();
         }
-      }, _apiGeneric, this, [[3, 11]]);
-    })
-  );
+      }
+    }, _apiGeneric, this, [[3, 11]]);
+  });
 };
 
 var watchFetch = function watchFetch(apiClient) {
-  return (/*#__PURE__*/_regenerator2.default.mark(function _watchFetch() {
-      return _regenerator2.default.wrap(function _watchFetch$(_context3) {
-        while (1) {
-          switch (_context3.prev = _context3.next) {
-            case 0:
-              return _context3.delegateYield((0, _effects.takeEvery)(_actionTypes.FETCH, apiGeneric(apiClient)), 't0', 1);
+  return _regenerator2.default.mark(function _watchFetch() {
+    return _regenerator2.default.wrap(function _watchFetch$(_context3) {
+      while (1) {
+        switch (_context3.prev = _context3.next) {
+          case 0:
+            return _context3.delegateYield((0, _effects.takeEvery)(_actionTypes.FETCH, apiGeneric(apiClient)), 't0', 1);
 
-            case 1:
-            case 'end':
-              return _context3.stop();
-          }
+          case 1:
+          case 'end':
+            return _context3.stop();
         }
-      }, _watchFetch, this);
-    })
-  );
+      }
+    }, _watchFetch, this);
+  });
 };
 
 var watchFetchOne = function watchFetchOne(apiClient) {
-  return (/*#__PURE__*/_regenerator2.default.mark(function _watchFetchOne() {
-      return _regenerator2.default.wrap(function _watchFetchOne$(_context4) {
-        while (1) {
-          switch (_context4.prev = _context4.next) {
-            case 0:
-              return _context4.delegateYield((0, _effects.takeEvery)(_actionTypes.FETCH_ONE, apiGeneric(apiClient)), 't0', 1);
+  return _regenerator2.default.mark(function _watchFetchOne() {
+    return _regenerator2.default.wrap(function _watchFetchOne$(_context4) {
+      while (1) {
+        switch (_context4.prev = _context4.next) {
+          case 0:
+            return _context4.delegateYield((0, _effects.takeEvery)(_actionTypes.FETCH_ONE, apiGeneric(apiClient)), 't0', 1);
 
-            case 1:
-            case 'end':
-              return _context4.stop();
-          }
+          case 1:
+          case 'end':
+            return _context4.stop();
         }
-      }, _watchFetchOne, this);
-    })
-  );
+      }
+    }, _watchFetchOne, this);
+  });
 };
 
 var watchCreate = function watchCreate(apiClient) {
-  return (/*#__PURE__*/_regenerator2.default.mark(function _watchCreate() {
-      return _regenerator2.default.wrap(function _watchCreate$(_context5) {
-        while (1) {
-          switch (_context5.prev = _context5.next) {
-            case 0:
-              return _context5.delegateYield((0, _effects.takeEvery)(_actionTypes.CREATE, apiGeneric(apiClient)), 't0', 1);
+  return _regenerator2.default.mark(function _watchCreate() {
+    return _regenerator2.default.wrap(function _watchCreate$(_context5) {
+      while (1) {
+        switch (_context5.prev = _context5.next) {
+          case 0:
+            return _context5.delegateYield((0, _effects.takeEvery)(_actionTypes.CREATE, apiGeneric(apiClient)), 't0', 1);
 
-            case 1:
-            case 'end':
-              return _context5.stop();
-          }
+          case 1:
+          case 'end':
+            return _context5.stop();
         }
-      }, _watchCreate, this);
-    })
-  );
+      }
+    }, _watchCreate, this);
+  });
 };
 
 var watchUpdate = function watchUpdate(apiClient) {
-  return (/*#__PURE__*/_regenerator2.default.mark(function _watchUpdate() {
-      return _regenerator2.default.wrap(function _watchUpdate$(_context6) {
-        while (1) {
-          switch (_context6.prev = _context6.next) {
-            case 0:
-              return _context6.delegateYield((0, _effects.takeEvery)(_actionTypes.UPDATE, apiGeneric(apiClient)), 't0', 1);
+  return _regenerator2.default.mark(function _watchUpdate() {
+    return _regenerator2.default.wrap(function _watchUpdate$(_context6) {
+      while (1) {
+        switch (_context6.prev = _context6.next) {
+          case 0:
+            return _context6.delegateYield((0, _effects.takeEvery)(_actionTypes.UPDATE, apiGeneric(apiClient)), 't0', 1);
 
-            case 1:
-            case 'end':
-              return _context6.stop();
-          }
+          case 1:
+          case 'end':
+            return _context6.stop();
         }
-      }, _watchUpdate, this);
-    })
-  );
+      }
+    }, _watchUpdate, this);
+  });
 };
 
 var watchDelete = function watchDelete(apiClient) {
-  return (/*#__PURE__*/_regenerator2.default.mark(function _watchDelete() {
-      return _regenerator2.default.wrap(function _watchDelete$(_context7) {
-        while (1) {
-          switch (_context7.prev = _context7.next) {
-            case 0:
-              return _context7.delegateYield((0, _effects.takeEvery)(_actionTypes.DELETE, apiGeneric(apiClient)), 't0', 1);
+  return _regenerator2.default.mark(function _watchDelete() {
+    return _regenerator2.default.wrap(function _watchDelete$(_context7) {
+      while (1) {
+        switch (_context7.prev = _context7.next) {
+          case 0:
+            return _context7.delegateYield((0, _effects.takeEvery)(_actionTypes.DELETE, apiGeneric(apiClient)), 't0', 1);
 
-            case 1:
-            case 'end':
-              return _context7.stop();
-          }
+          case 1:
+          case 'end':
+            return _context7.stop();
         }
-      }, _watchDelete, this);
-    })
-  );
+      }
+    }, _watchDelete, this);
+  });
 };
 
 var watchApiCall = function watchApiCall(apiClient) {
-  return (/*#__PURE__*/_regenerator2.default.mark(function _watchApiCall() {
-      return _regenerator2.default.wrap(function _watchApiCall$(_context8) {
-        while (1) {
-          switch (_context8.prev = _context8.next) {
-            case 0:
-              return _context8.delegateYield((0, _effects.takeEvery)(_actionTypes.API_CALL, apiGeneric(apiClient)), 't0', 1);
+  return _regenerator2.default.mark(function _watchApiCall() {
+    return _regenerator2.default.wrap(function _watchApiCall$(_context8) {
+      while (1) {
+        switch (_context8.prev = _context8.next) {
+          case 0:
+            return _context8.delegateYield((0, _effects.takeEvery)(_actionTypes.API_CALL, apiGeneric(apiClient)), 't0', 1);
 
-            case 1:
-            case 'end':
-              return _context8.stop();
-          }
+          case 1:
+          case 'end':
+            return _context8.stop();
         }
-      }, _watchApiCall, this);
-    })
-  );
+      }
+    }, _watchApiCall, this);
+  });
 };
 
 function crudSaga(apiClient) {
-  return (/*#__PURE__*/_regenerator2.default.mark(function _crudSaga() {
-      return _regenerator2.default.wrap(function _crudSaga$(_context9) {
-        while (1) {
-          switch (_context9.prev = _context9.next) {
-            case 0:
-              _context9.next = 2;
-              return [(0, _effects.fork)(watchFetch(apiClient)), (0, _effects.fork)(watchFetchOne(apiClient)), (0, _effects.fork)(watchCreate(apiClient)), (0, _effects.fork)(watchUpdate(apiClient)), (0, _effects.fork)(watchDelete(apiClient)), (0, _effects.fork)(watchApiCall(apiClient)), (0, _effects.fork)(garbageCollector)];
+  return _regenerator2.default.mark(function _crudSaga() {
+    return _regenerator2.default.wrap(function _crudSaga$(_context9) {
+      while (1) {
+        switch (_context9.prev = _context9.next) {
+          case 0:
+            _context9.next = 2;
+            return [(0, _effects.fork)(watchFetch(apiClient)), (0, _effects.fork)(watchFetchOne(apiClient)), (0, _effects.fork)(watchCreate(apiClient)), (0, _effects.fork)(watchUpdate(apiClient)), (0, _effects.fork)(watchDelete(apiClient)), (0, _effects.fork)(watchApiCall(apiClient)), (0, _effects.fork)(garbageCollector)];
 
-            case 2:
-            case 'end':
-              return _context9.stop();
-          }
+          case 2:
+          case 'end':
+            return _context9.stop();
         }
-      }, _crudSaga, this);
-    })
-  );
+      }
+    }, _crudSaga, this);
+  });
 }
